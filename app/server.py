e2cd35c0552d222378db4908223aa4c171107a57
Nishant Supakar <supakar.nishant@gmail.com>
2020-05-15 10:36:11 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1OvLBQo1OxgmCdyi0O8n8m97bLZ5CpgXk?alt=media&key=AIzaSyBWAb8muNN4Eob8Cz8mInke3S8BTHnd0Jo'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Jerry Seinfeld', 'George Costanza', 'Elaine Benes', 'Estelle Costanza', 'Cosmo Kramer', 'Newman',' Frank Costanza']
 path = Path(__file__).parent
 
 app = Starlette()
